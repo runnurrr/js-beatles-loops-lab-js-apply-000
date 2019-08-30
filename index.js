1a3ab5da7cd5d091b4bcca338386050f89a8eb10
Nuri Park <nuripark@gmail.com>
2019-08-30 18:06:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,14 @@
 function theBeatlesPlay(musicians, instruments) {
   let musicianInstArr = [];
   for (let i = 0; i < musicians.length; i++) {
-    musiciansInstArr.push(``musicians[i])
+    musiciansInstArr.push(`$musicians[i] plays $instruments[i]`);
   }
-  
+  return musicianInstArr;
+}
+
+function johnLennonFacts(factsArr) {
+  for (let i = 0; i < factsArr.length; i++){
+    factsArr[i] = factsArr[i] + "!!!";
+  }
+  return factsArr;
 }
