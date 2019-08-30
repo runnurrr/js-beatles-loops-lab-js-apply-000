290b194bc5c3bf8af06c68996b373c4b59da3992
Nuri Park <nuripark@gmail.com>
2019-08-30 18:04:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  let musicianInstArr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    musiciansInstArr.push(``musicians[i])
+  }
+  
+}
